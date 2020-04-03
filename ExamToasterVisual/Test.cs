96e4a9af0b8aec91c4aae97efdf7582392cd7729
Dreamer <dreamerandprogrammer@gmail.com>
2020-04-03 20:39:51 +0300

    Add Test Class to Main Project

---
+++ ExamToasterVisual/Test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExamToasterConsole
+{
+    public class Variant
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public double rating { get; set; }
+    }
+
+    public class Question
+    {
+        public string title { get; set; }
+        public string text { get; set; }
+        public string image { get; set; }
+        public bool multiply { get; set; }
+        public List<Variant> variants { get; set; }
+    }
+
+    public class Rating
+    {
+        public string title { get; set; }
+        public double rating { get; set; }
+    }
+
+    public class Test
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public string author { get; set; }
+        public string image { get; set; }
+        public List<Question> questions { get; set; }
+        public List<Rating> ratings { get; set; }
+    }
+}
---
+++ ExamToasterVisual/Toaster.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Text.Json;
+using System.IO.Compression;
+
+namespace ExamToasterConsole
+{
+    class Toaster
+	{
+        public Test test;
+
+        private string file_path = @"D:\TOSTER_test.tst";
+        private string extract_path = @".\extract\";
+
+        double rating = 0;
+        int n_question = 0;
+
+        public Toaster()
+        {
+            //
+        }
+
+        public bool OpenFile(string path)
+        {
+            this.file_path = path;
+
+            if (Directory.Exists(extract_path)) Directory.Delete(extract_path, true);
+
+            ZipFile.ExtractToDirectory(file_path, extract_path);
+
+            string jsonString = File.ReadAllText(extract_path + "test.json");
+
+            test = JsonSerializer.Deserialize<Test>(jsonString);
+
+            return true;
+        }
+
+        public void StartTest()
+        {
+            //
+        }
+
+        public void Question(int n)
+        {
+            //
+        }
+    }
+}
