921ab0e3891bb971d4561ebe0195cba65e5e1173
Dreamer <dreamerandprogrammer@gmail.com>
2020-04-04 14:13:16 +0300

    Lists withs variant UIs

--- ExamToasterVisual/Form1.cs
+++ ExamToasterVisual/Form1.cs
@@ -14,6 +14,9 @@ namespace ExamToasterVisual
 	{
 		static Toaster toaster = new Toaster();
 
+		List<RadioButton> rbs = new List<RadioButton> { };
+		List<CheckBox> cbs = new List<CheckBox> { };
+
 		public Form1()
 		{
 			InitializeComponent();
@@ -26,6 +29,26 @@ namespace ExamToasterVisual
 			btnPrevious.Visible = false;
 
 			lblCaption.Text = "ExamToaster by DeLy";
+
+			rbs = new List<RadioButton>
+			{
+				rbVariant1,
+				rbVariant2,
+				rbVariant3,
+				rbVariant4,
+				rbVariant5,
+				rbVariant6
+			};
+
+			cbs = new List<CheckBox> 
+			{ 
+				cbVariant1,
+				cbVariant2,
+				cbVariant3,
+				cbVariant4,
+				cbVariant5,
+				cbVariant6
+			};
 		}
 
 		private void btnStart_Click(object sender, EventArgs e)
