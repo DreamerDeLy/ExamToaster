b7ab3109f4dcebb120ced0795b52cebb815644f2
Dreamer <dreamerandprogrammer@gmail.com>
2020-04-04 14:12:43 +0300

    Spaces to tabs

--- ExamToasterVisual/Test.cs
+++ ExamToasterVisual/Test.cs
@@ -3,35 +3,35 @@ using System.Collections.Generic;
 
 namespace ExamToasterVisual
 {
-    public class Variant
-    {
-        public string title { get; set; }
-        public string description { get; set; }
-        public double rating { get; set; }
-    }
+	public class Variant
+	{
+		public string title { get; set; }
+		public string description { get; set; }
+		public double rating { get; set; }
+	}
 
-    public class Question
-    {
-        public string title { get; set; }
-        public string text { get; set; }
-        public string image { get; set; }
-        public bool multiply { get; set; }
-        public List<Variant> variants { get; set; }
-    }
+	public class Question
+	{
+		public string title { get; set; }
+		public string text { get; set; }
+		public string image { get; set; }
+		public bool multiply { get; set; }
+		public List<Variant> variants { get; set; }
+	}
 
-    public class Rating
-    {
-        public string title { get; set; }
-        public double rating { get; set; }
-    }
+	public class Rating
+	{
+		public string title { get; set; }
+		public double rating { get; set; }
+	}
 
-    public class Test
-    {
-        public string title { get; set; }
-        public string description { get; set; }
-        public string author { get; set; }
-        public string image { get; set; }
-        public List<Question> questions { get; set; }
-        public List<Rating> ratings { get; set; }
-    }
+	public class Test
+	{
+		public string title { get; set; }
+		public string description { get; set; }
+		public string author { get; set; }
+		public string image { get; set; }
+		public List<Question> questions { get; set; }
+		public List<Rating> ratings { get; set; }
+	}
 }
